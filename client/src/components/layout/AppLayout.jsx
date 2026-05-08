9fe230fb4530bc4c827d6aa744a1e3e41c96a4b2
Muhammad-Husnain07 <muhammad.husnain.dev@gmail.com>
2026-05-08 16:03:39 +0500

    fix: use slate-950 for dark mode background in AppLayout

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 
 const AppLayout = ({ title, breadcrumbs, children, rightContent }) => {
   return (
-    <div className="flex min-h-screen bg-gray-50/80 dark:bg-gray-900/80">
+    <div className="flex min-h-screen bg-gray-50/80 dark:bg-slate-950/80">
       <Sidebar />
       <div className="flex-1 flex flex-col ml-60 min-w-0">
         <Navbar title={title} breadcrumbs={breadcrumbs} rightContent={rightContent} />
